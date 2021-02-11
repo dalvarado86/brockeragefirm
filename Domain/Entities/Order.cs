0f7d26524627b52a003d1036d75576982ca6dc09
David Alvarado <dadabit@gmail.com>
2021-02-11 10:47:12 -0600

    Adding base domain entities

--- Domain/Entities/Order.cs
+++ Domain/Entities/Order.cs
@@ -3,6 +3,8 @@
     public class Order
     {
         public int TimeStamp { get; set; }
+        public int AccountId { get; set; }
+        public Account Account { get; set; }
         public string Operation { get; set; }
         public Issuer Issuer { get; set; }
     }
