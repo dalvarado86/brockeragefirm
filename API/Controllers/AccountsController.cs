ce503c9c0b37a92e79202792e709f38b3f152ba1
David Alvarado <dadabit@gmail.com>
2021-02-12 15:50:04 -0600

    Adding orders

--- API/Controllers/AccountsController.cs
+++ API/Controllers/AccountsController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Accounts;
+using Application.Orders;
 using Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
@@ -8,9 +9,19 @@ namespace API.Controllers
     public class AccountsController : ApiControllerBase
     {
         [HttpPost]
-        public async Task<ActionResult<Account>> Add([FromBody]CreateAccountCommand command)
+        public async Task<ActionResult<Account>> CreateAccount(CreateAccountCommand command)
         {
             return await Mediator.Send(command);
-        }       
+        } 
+        
+        [HttpPost("{id}/orders")]
+        public async Task<ActionResult<MediatR.Unit>> SendOrder(int id, CreateOrderCommand command)
+        {
+            if (id < 0)
+                return BadRequest(new { id = "AccountId must not be empty." });
+                
+            command.AccountId = id;
+            return await Mediator.Send(command);
+        }
     }
 }
---
+++ Application/Orders/CreateOrderCommand.cs
@@ -0,0 +1,81 @@
+﻿using Domain.Entities;
+using FluentValidation;
+using Infrastructure.Persistence;
+using MediatR;
+using System;
+using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Orders
+{
+    public class CreateOrderCommand : IRequest
+    {
+        [JsonIgnore]
+        public int AccountId { get; set; }
+        public long Timestamp { get; set; }
+        public string Operation { get; set; }   
+        
+        [JsonPropertyName("issuer_name")]
+        public string IssuerName { get; set; }
+
+        [JsonPropertyName("total_shares")]
+        public int TotalShares { get; set; }
+
+        [JsonPropertyName("share_price")]
+        public decimal SharePrice { get; set; }
+    }
+
+    public class Handler : IRequestHandler<CreateOrderCommand>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Handler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public class CommandValidator : AbstractValidator<CreateOrderCommand>
+        {
+            public CommandValidator()
+            {
+                //RuleFor(x => x.AccountId).NotEmpty();
+                RuleFor(x => x.Operation).NotEmpty();
+                RuleFor(x => x.IssuerName).NotEmpty();
+                RuleFor(x => x.TotalShares)
+                    .NotEmpty()
+                    .GreaterThan(0);
+                RuleFor(x => x.SharePrice)
+                   .NotEmpty()
+                   .GreaterThan(0);
+            }
+        }
+
+        public async Task<Unit> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {            
+            var account = await _context.Accounts.FindAsync(request.AccountId);
+
+            if (account == null)
+                throw new Exception("Account not found");
+
+            var order = new Order
+            {
+                AccountId = request.AccountId,
+                TimeStamp = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds(),
+                Operation = request.Operation,
+                IssuerName = request.IssuerName,
+                TotalShares = request.TotalShares,
+                SharePrice = request.SharePrice
+            };
+
+            _context.Orders.Add(order);
+
+            var success = await _context.SaveChangesAsync(cancellationToken) > 0;
+
+            if (success)
+                return Unit.Value;
+
+            throw new Exception("Problem saving changes");
+        }
+    }
+}
